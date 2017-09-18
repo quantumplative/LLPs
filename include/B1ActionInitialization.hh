f981c3ebcb0259ce4113637ac8efa71f23c8f142
Akaxia Cruz <admcruz@tev09.phys.washington.edu>
2017-09-18 12:14:12 -0700

    GEANT4 source files for Mathusla with mono-energetic muons

---
+++ include/B1ActionInitialization.hh
@@ -0,0 +1,25 @@
+/// \file B1ActionInitialization.hh
+/// \brief Definition of the B1ActionInitialization class
+
+#ifndef B1ActionInitialization_h
+#define B1ActionInitialization_h 1
+
+#include "G4VUserActionInitialization.hh"
+
+/// Action initialization class.
+
+class B1ActionInitialization : public G4VUserActionInitialization
+{
+  public:
+    B1ActionInitialization();
+    virtual ~B1ActionInitialization();
+
+    virtual void BuildForMaster() const;
+    virtual void Build() const;
+};
+
+//------------------------------------------------------------------------------
+
+#endif
+
+    
